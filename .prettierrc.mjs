a3598886787f6fd966759e4f10110349157e10f1
Roman <22231294+Lutymane@users.noreply.github.com>
2024-06-23 02:27:36 +0500

    chore: prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Options}
+ */
+export default {
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 80,
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "es5",
+  vueIndentScriptAndStyle: false,
+};
